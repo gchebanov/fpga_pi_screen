3ba3168fd2ae316bd2eacd42f4c7a5f39cf81a85
Georgy Chebanov <gchebanov@gmail.com>
2019-11-15 00:18:20 +0300

    fits timings (WNS from -60ns to +12ns)

--- pi_get_digit.v
+++ pi_get_digit.v
@@ -4,36 +4,27 @@ module pi_get_digit # (
 	input wire clk,
 	input wire rst,
 	input wire [N-1:0] pi_index,
-	output wire [3:0] pi_digit
+	output logic [3:0] pi_digit
 );
 
 localparam MEM_W = 36;
-localparam D = 0;
-reg [N-1:0] addr [D:0];
-reg [N-1:0] addr_base [D:0];
-reg [N-1:0] digit_base [D:0];
-reg [N-1:0] digit_base10 [D:0];
-reg [5:0] addr_shift [D:0];
-reg [2:0] digit_shift [D:0];
 
-always @(posedge clk) begin
-	for (int i = 1; i <= D; i++) begin
-		addr[i] <= addr[i-1];
-		addr_base[i] <= addr_base[i-1];
-		digit_base[i] <= digit_base[i-1];
-		digit_base10[i] <= digit_base10[i-1];
-		addr_shift[i] <= addr_shift[i-1];
-		digit_shift[i] <= digit_shift[i-1];
-	end
-end
+reg [N-1:0] addr;
+reg [N-1:0] addr_base;
+reg [N-1:0] addr_base_unshift;
+reg [N-1:0] digit_base;
+reg [N-1:0] digit_base_unshift;
+reg [N-1:0] digit_base10;
+reg [5:0] addr_shift;
+reg [1:0] digit_shift;
 
 wire [MEM_W-1:0] mem_q;
-pi_mem pi_mem1 (clk, addr[0], mem_q);
+pi_mem pi_mem1 (clk, addr, mem_q);
 
 reg [9:0] dpd_b_in;
 reg [3:0] dpd_d_out [2:0];
 dpd_decoder dpd_ins0(dpd_b_in, dpd_d_out);
-assign pi_digit = dpd_d_out[2 - digit_shift[D]];
+assign pi_digit = dpd_d_out[2 - digit_shift];
 
 reg cnt;
 reg [MEM_W-1:0] mem_a, mem_b;
@@ -60,14 +51,16 @@ always @(posedge clk) begin
 		dpd_b_in <= '0;
 	end else begin
 		cnt <= ~cnt;
-		addr[0] <= addr_base[D] + cnt;
-		addr_base[0] <= digit_base10[D] / MEM_W;
-		addr_shift[0] <= digit_base10[D] % MEM_W;
-		digit_shift[0] <= pi_index % 3;
-		digit_base[0] <= pi_index / 3;
-		digit_base10[0] <= digit_base[D] * 10;
+		addr <= addr_base + cnt;
+		addr_base <= ((digit_base10 / 4) * 32'd29128) / 19'h4_0000;
+		addr_base_unshift <= addr_base * MEM_W;
+		addr_shift <= digit_base10 - addr_base_unshift;
+		digit_base_unshift <= digit_base * 2'd3;
+		digit_shift <= pi_index - digit_base_unshift;
+		digit_base <= (pi_index * 32'd87382) / 19'h4_0000;
+		digit_base10 <= digit_base * 4'd10;
 		mem_ab <= {mem_b, mem_a};
-		dpd_b_in <= mem_ab[addr_shift[D] +: 10];
+		dpd_b_in <= mem_ab[addr_shift +: 10];
 	end
 end
 
