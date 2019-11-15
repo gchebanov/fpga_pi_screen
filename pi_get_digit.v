7fad0d4c0e67b680d220b90f8a52c69165a12c1c
Georgy Chebanov <gchebanov@gmail.com>
2019-11-15 22:01:39 +0300

    more pipeline

--- pi_get_digit.v
+++ pi_get_digit.v
@@ -2,7 +2,6 @@ module pi_get_digit # (
 	parameter N = 17
 ) (
 	input wire clk,
-	input wire rst,
 	input wire [N-1:0] pi_index,
 	output logic [3:0] pi_digit
 );
@@ -26,42 +25,62 @@ reg [3:0] dpd_d_out [2:0];
 dpd_decoder dpd_ins0(dpd_b_in, dpd_d_out);
 assign pi_digit = dpd_d_out[2 - digit_shift];
 
-reg cnt;
+reg[17:0] mul_a, mul_b;
+reg[35:0] mul_ab;
+
+always @(posedge clk) begin
+	mul_ab <= mul_a * mul_b;
+end
+
+reg [1:0] cnt;
 reg [MEM_W-1:0] mem_a, mem_b;
 reg [MEM_W*2-1:0] mem_ab;
 
 always @(posedge clk) begin
-	if (cnt) begin
+	case (cnt[0])
+	0: begin
 		mem_a <= mem_q;
-	end else begin
-		mem_b <= mem_q;
+		addr <= addr_base + 1'b1;
 	end
+	1: begin
+		mem_b <= mem_q;
+		addr <= addr_base;
+	end
+	endcase
 end
 
 always @(posedge clk) begin
-	if (rst) begin
-		cnt <= 0;
-		addr[0] <= 0;
-		addr_base[0] <= 0;
-		addr_shift[0] <= 0;
-		digit_shift[0] <= 0;
-		digit_base[0] <= 0;
-		digit_base10[0] <= 0;
-		mem_ab <= '0;
-		dpd_b_in <= '0;
-	end else begin
-		cnt <= ~cnt;
-		addr <= addr_base + cnt;
-		addr_base <= ((digit_base10 / 4) * 32'd29128) / 19'h4_0000;
-		addr_base_unshift <= addr_base * MEM_W;
-		addr_shift <= digit_base10 - addr_base_unshift;
-		digit_base_unshift <= digit_base * 2'd3;
-		digit_shift <= pi_index - digit_base_unshift;
-		digit_base <= (pi_index * 32'd87382) / 19'h4_0000;
-		digit_base10 <= digit_base * 4'd10;
-		mem_ab <= {mem_b, mem_a};
-		dpd_b_in <= mem_ab[addr_shift +: 10];
+	case (cnt)
+	0: begin
+		mul_a <= digit_base10 >> 2;
+		mul_b <= 29128;
+		addr_base_unshift <= mul_ab;
 	end
+	1: begin
+		mul_a <= pi_index;
+		mul_b <= 87382;
+		digit_base10 <= mul_ab;
+	end
+	2: begin
+		mul_a <= addr_base;
+		mul_b <= MEM_W;
+		addr_base <= mul_ab[35:18];
+	end
+	3: begin
+		mul_a <= digit_base;
+		mul_b <= 4'd10;
+		digit_base <= mul_ab[35:18];
+	end
+	endcase
+end
+
+always @(posedge clk) begin
+	cnt <= cnt + 1;
+	addr_shift <= digit_base10 - addr_base_unshift;
+	digit_shift <= pi_index - digit_base_unshift;
+	digit_base_unshift <= digit_base * 2'd3;
+	mem_ab <= {mem_b, mem_a};
+	dpd_b_in <= mem_ab[addr_shift +: 10];
 end
 
 endmodule // pi_get_digit
